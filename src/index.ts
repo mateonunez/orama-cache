a8b5c1434161612b01a2ca6efdb89d05b5eac99c
mateonunez <mateonunez95@gmail.com>
2022-10-24 14:11:51 +0200

    feat(*): add simple searchCache implementation

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,20 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import {createCache} from "async-cache-dedupe"
-import {create, insert, search} from "@lyrasearch/lyra"
-import {formatNanoseconds, getNanosecondsTime} from "@lyrasearch/lyra/dist/cjs/src/utils"
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+import {Lyra, PropertiesSchema, SearchResult, search} from "@lyrasearch/lyra"
+
+type SearchCacheOptions<S extends PropertiesSchema> = {db: Lyra<S>; term: string}
 
 const cache = createCache({
-  ttl: 3,
+  ttl: 1000,
   storage: {type: "memory"}
 })
 
-const db = create({
-  schema: {
-    author: "string",
-    quote: "string"
-  }
-})
-
-
-cache.define("search", {}, async (term: string) => {
+cache.define("search", {}, async <S extends PropertiesSchema>({db, term}: SearchCacheOptions<S>) => {
   return search(db, {term})
 })
 
-async function main() {
-  for (const _ of Array(10000).keys()) {
-    insert(db, {
-      author: "John Doe",
-      quote: `Hello World! ${_}`
-    })
-  }
+export function searchCache<S extends PropertiesSchema>(options: SearchCacheOptions<S>): Promise<SearchResult<S>> {
+  if (!options.db) throw new Error("db is required")
+  if (!options.term) throw new Error("term is required")
 
-  const startTime = getNanosecondsTime()
-  const fetchRes = await cache.search("Hello")
-  console.log({
-    elapsedTime: formatNanoseconds(getNanosecondsTime() - startTime),
-    fetchRes
-  })
-
-  const startTimeCached = getNanosecondsTime()
-  const fetchCached = await cache.search("Hello")
-  console.log({
-    elapsedTimeCached: formatNanoseconds(getNanosecondsTime() - startTimeCached),
-    fetchCached
-  })
-
-  const startTimeWithoutCache = getNanosecondsTime()
-  const fetchWithoutCache = search(db, {term: "Hello"})
-  console.log({
-    elapsedTimeWithoutCache: formatNanoseconds(getNanosecondsTime() - startTimeWithoutCache),
-    fetchWithoutCache
-  })
-
-  await sleep(4000)
-
-  const startTimeAfterTTL = getNanosecondsTime()
-  const fetchAfterTTL = await cache.search("Hello")
-  console.log({
-    elapsedTimeAfterTTL: formatNanoseconds(getNanosecondsTime() - startTimeAfterTTL),
-    fetchAfterTTL
-  })
-
-  const startTimeCachedAfterTTL = getNanosecondsTime()
-  const fetchCachedAfterTTL = await cache.search("Hello")
-  console.log({
-    elapsedTimeCachedAfterTTL: formatNanoseconds(getNanosecondsTime() - startTimeCachedAfterTTL),
-    fetchCachedAfterTTL
-  })
+  return cache.search(options) as Promise<SearchResult<S>>
 }
-
-main()
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,5 +1,31 @@
-import {test} from "tap"
+import { create, insert } from "@lyrasearch/lyra"
+import { searchCache } from ".."
+import { test } from "tap"
 
-test("true", async ({ok}) => {
-  ok(true)
+test("cache", async ({ test }) => {
+  test("should cache search results", async ({ equal, same }) => {
+    const db = create({ schema: { name: "string" } })
+    insert(db, { name: "foo" })
+    insert(db, { name: "bar" })
+
+    const results1 = await searchCache({ db, term: "foo" })
+    const resultsCached = await searchCache({ db, term: "foo" })
+
+    same(results1, resultsCached)
+
+    const results2 = await searchCache({ db, term: "bar" })
+    equal(results2.count, 1)
+  })
+})
+
+test("errors", async ({ test }) => {
+  test("should throw an error if db is not provided", async ({ throws }) => {
+    // @ts-expect-error - db is required
+    throws(() => searchCache({ term: "foo" }), /db is required/)
+  })
+
+  test("should throw an error if term is not provided", async ({ throws }) => {
+    // @ts-expect-error - term is required
+    throws(() => searchCache({ db: create({ schema: {} }) }), /term is required/)
+  })
 })
