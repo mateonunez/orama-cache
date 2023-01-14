6c8b215f9fe6fac214f3e0d6878d941ef43c6c21
mateonunez <mateonunez95@gmail.com>
2023-01-14 12:07:42 +0100

    test: resolve promises and fix tests

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,13 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {create, insert} from "@lyrasearch/lyra"
 import {createLyraCache, searchCache} from ".."
 import {test} from "tap"
 
 test("cache", async ({test}) => {
   test("should cache search results", async ({equal, same}) => {
-    const db = create({schema: {name: "string"}})
-    const lyraCache = createLyraCache()
-    insert(db, {name: "foo"})
-    insert(db, {name: "bar"})
+    const db = await create({schema: {name: "string"}})
+    const lyraCache = await createLyraCache()
+    await insert(db, {name: "foo"})
+    await insert(db, {name: "bar"})
 
     const results1 = await searchCache({db, term: "foo"}, lyraCache)
     const resultsCached = await searchCache({db, term: "foo"}, lyraCache)
@@ -20,30 +21,42 @@ test("cache", async ({test}) => {
 })
 
 test("errors", async ({test}) => {
-  const lyraCache = createLyraCache()
+  const lyraCache = await createLyraCache()
 
-  test("should throw an error if cache is not provided", async ({throws}) => {
-    // @ts-expect-error - cache is required
-    throws(() => searchCache({db: create({schema: {}}), term: "foo"}), /cache is required/)
+  test("should throw an error if cache is not provided", async ({same}) => {
+    try {
+      // @ts-expect-error - cache is required
+      await searchCache({db: await create({schema: {}}), term: "foo"})
+    } catch (error: any) {
+      same(error.message, "cache is required")
+    }
   })
 
-  test("should throw an error if db is not provided", async ({throws}) => {
-    // @ts-expect-error - db is required
-    throws(() => searchCache({term: "foo"}, lyraCache), /db is required/)
+  test("should throw an error if db is not provided", async ({same}) => {
+    try {
+      // @ts-expect-error - db is required
+      await searchCache({term: "foo"}, lyraCache)
+    } catch (error: any) {
+      same(error.message, "db is required")
+    }
   })
 
-  test("should throw an error if term is not provided", async ({throws}) => {
-    // @ts-expect-error - term is required
-    throws(() => searchCache({db: create({schema: {}})}, lyraCache), /term is required/)
+  test("should throw an error if term is not provided", async ({same}) => {
+    try {
+      // @ts-expect-error - term is required
+      await searchCache({db: await create({schema: {}})}, lyraCache)
+    } catch (error: any) {
+      same(error.message, "term is required")
+    }
   })
 })
 
 test("should cache results with multiple Lyra instances", async ({equal}) => {
-  const db1 = create({schema: {name: "string"}})
-  const db2 = create({schema: {description: "string"}})
-  const lyraCache = createLyraCache()
-  insert(db1, {name: "foo"})
-  insert(db2, {description: "foo"})
+  const db1 = await create({schema: {name: "string"}})
+  const db2 = await create({schema: {description: "string"}})
+  const lyraCache = await createLyraCache()
+  await insert(db1, {name: "foo"})
+  await insert(db2, {description: "foo"})
 
   const results1 = await searchCache({db: db1, term: "foo"}, lyraCache)
   const results2 = await searchCache({db: db2, term: "foo"}, lyraCache)
