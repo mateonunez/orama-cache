291677e93cf27f119393a9547e3b2fec1a1cf3ac
mateonunez <mateonunez95@gmail.com>
2023-01-20 16:02:44 +0100

    test(lyra-cache): fix and add cache tests

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,66 +1,107 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {create, insert} from "@lyrasearch/lyra"
-import {createLyraCache, searchCache} from ".."
+import {createLyraCache} from ".."
 import {test} from "tap"
+import {promisify} from "util"
+
+const sleep = promisify(setTimeout)
 
 test("cache", async ({test}) => {
-  test("should cache search results", async ({equal, same}) => {
+  test("should cache search results", async ({plan, same}) => {
+    plan(2)
+
     const db = await create({schema: {name: "string"}})
-    const lyraCache = await createLyraCache()
+    const cache = await createLyraCache(db)
     await insert(db, {name: "foo"})
     await insert(db, {name: "bar"})
 
-    const results1 = await searchCache({db, term: "foo"}, lyraCache)
-    const resultsCached = await searchCache({db, term: "foo"}, lyraCache)
+    const results1 = await cache.search({term: "foo"})
+    const resultsCached = await cache.search({term: "foo"})
 
     same(results1, resultsCached)
 
-    const results2 = await searchCache({db, term: "bar"}, lyraCache)
-    equal(results2.count, 1)
+    const results2 = await cache.search({term: "bar"})
+    same(results2.count, 1)
   })
 })
 
-test("errors", async ({test}) => {
-  const lyraCache = await createLyraCache()
+test("should cache results with multiple Lyra instances", async ({plan, same}) => {
+  plan(2)
 
-  test("should throw an error if cache is not provided", async ({same}) => {
-    try {
-      // @ts-expect-error - cache is required
-      await searchCache({db: await create({schema: {}}), term: "foo"})
-    } catch (error: any) {
-      same(error.message, "cache is required")
-    }
-  })
-
-  test("should throw an error if db is not provided", async ({same}) => {
-    try {
-      // @ts-expect-error - db is required
-      await searchCache({term: "foo"}, lyraCache)
-    } catch (error: any) {
-      same(error.message, "db is required")
-    }
-  })
-
-  test("should throw an error if term is not provided", async ({same}) => {
-    try {
-      // @ts-expect-error - term is required
-      await searchCache({db: await create({schema: {}})}, lyraCache)
-    } catch (error: any) {
-      same(error.message, "term is required")
-    }
-  })
-})
-
-test("should cache results with multiple Lyra instances", async ({equal}) => {
   const db1 = await create({schema: {name: "string"}})
   const db2 = await create({schema: {description: "string"}})
-  const lyraCache = await createLyraCache()
+
+  const cache = await createLyraCache(db1)
+  const cache2 = await createLyraCache(db2)
+
   await insert(db1, {name: "foo"})
   await insert(db2, {description: "foo"})
 
-  const results1 = await searchCache({db: db1, term: "foo"}, lyraCache)
-  const results2 = await searchCache({db: db2, term: "foo"}, lyraCache)
+  const results1 = await cache.search({term: "foo"})
+  const results2 = await cache2.search({term: "foo"})
 
-  equal(results1.count, 1)
-  equal(results1.count, results2.count)
+  same(results1.count, 1)
+  same(results1.count, results2.count)
+})
+
+test("should hit a cached key", async ({plan, same}) => {
+  plan(1)
+
+  const db = await create({schema: {name: "string"}})
+  const searchable = {term: "foo"}
+
+  const cache = await createLyraCache(db, {
+    onHit: (key: string) => same(key, JSON.stringify(searchable))
+  })
+
+  await insert(db, {name: "foo"})
+  await cache.search(searchable)
+  await cache.search(searchable)
+})
+
+test("should miss a cached key", async ({plan, same}) => {
+  plan(1)
+
+  const db = await create({schema: {name: "string"}})
+  const searchable = {term: "foo"}
+
+  const cache = await createLyraCache(db, {
+    onMiss: (key: string) => same(key, JSON.stringify(searchable))
+  })
+
+  await insert(db, {name: "foo"})
+  await cache.search(searchable)
+})
+
+test("ttl should expire a cached key", async ({same}) => {
+  const db = await create({schema: {name: "string"}})
+  const searchable = {term: "foo"}
+
+  const cache = await createLyraCache(db, {
+    ttl: 1,
+    onMiss: (key: string) => same(key, JSON.stringify(searchable))
+  })
+
+  await insert(db, {name: "foo"})
+  await cache.search(searchable)
+
+  await sleep(1500)
+
+  await cache.search(searchable)
+})
+
+test("clear should clear the cache", async ({same}) => {
+  const db = await create({schema: {name: "string"}})
+  const searchable = {term: "foo"}
+
+  const cache = await createLyraCache(db, {
+    onMiss: (key: string) => same(key, JSON.stringify(searchable))
+  })
+
+  await insert(db, {name: "foo"})
+  await cache.search(searchable)
+
+  cache.clear()
+
+  await cache.search(searchable)
 })
