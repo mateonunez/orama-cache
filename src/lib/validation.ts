1553394f051416620449ec5a784c553689d18e9b
mateonunez <mateonunez95@gmail.com>
2023-10-19 22:15:02 +0200

    refactor(validation)!: apply immutability
    Signed-off-by: mateonunez <mateonunez95@gmail.com>

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,81 +1,107 @@
-import {CreateCacheOptions, OptionsMemory, OptionsRedis, ValidatedCacheOptions} from "async-cache-dedupe"
+import {CreateCacheOptions, OptionsMemory, OptionsRedis, StorageType, ValidatedCacheOptions} from "async-cache-dedupe"
 
-export function validateOptions(options: CreateCacheOptions): ValidatedCacheOptions {
-  const validated = options
+export function validateOptions<T extends CreateCacheOptions>(options: T): ValidatedCacheOptions<Extract<T["storage"], {type: StorageType}>["type"]> {
+  let validated: CreateCacheOptions = {...options}
 
-  // TTL validation number and greater than 0
-  if (validated.ttl !== undefined && (validated.ttl < 0 || typeof validated.ttl !== "number")) {
-    throw new Error("ttl must be greater than 0")
-  } else if (validated.ttl === undefined) {
-    validated.ttl = 60
+  validated = validateTTL(validated)
+  validated = validateStale(validated)
+  validated = validateFunctionProperties(validated)
+  validated = validateStorage(validated)
+
+  return validated as ValidatedCacheOptions<Extract<T["storage"], {type: StorageType}>["type"]>
+}
+
+function validateTTL(options: CreateCacheOptions): CreateCacheOptions {
+  return {
+    ...options,
+    ttl: ensureNumberAndPositive(options.ttl, "ttl", 60)
   }
+}
 
-  // Stale validation number and greater than 0
-  if (validated.stale !== undefined && (validated.stale < 0 || typeof validated.stale !== "number")) {
-    throw new Error("stale must be greater than 0")
+function validateStale(options: CreateCacheOptions): CreateCacheOptions {
+  return {
+    ...options,
+    stale: ensureNumberAndPositive(options.stale, "stale", 60)
   }
+}
 
-  // onDedupe validation
-  if (options.onDedupe && typeof options.onDedupe !== "function") {
-    throw new Error("onDedupe must be a function")
-  }
+function validateFunctionProperties(options: CreateCacheOptions): CreateCacheOptions {
+  const functionProperties: (keyof CreateCacheOptions)[] = ["onDedupe", "onError", "onHit", "onMiss"]
 
-  // onError validation
-  if (options.onError && typeof options.onError !== "function") {
-    throw new Error("onError must be a function")
-  }
-
-  // onHit validation
-  if (options.onHit && typeof options.onHit !== "function") {
-    throw new Error("onHit must be a function")
-  }
-
-  // onMiss validation
-  if (options.onMiss && typeof options.onMiss !== "function") {
-    throw new Error("onMiss must be a function")
-  }
-
-  // Storage validation
-  if (options.storage) {
-    if (options.storage.type === "memory") {
-      const optionsMemory = options.storage.options as OptionsMemory
-
-      if (optionsMemory) {
-        if (optionsMemory.size && optionsMemory.size < 0) {
-          throw new Error("storage.options.size must be greater than 0")
-        }
-      } else if (!optionsMemory) {
-        validated.storage = {
-          type: "memory",
-          options: {
-            size: 1000
-          }
-        }
-      }
-    } else if (options.storage.type === "redis") {
-      const optionsRedis = options.storage.options as OptionsRedis
-
-      if (optionsRedis) {
-        if (!optionsRedis.client || (optionsRedis.client && typeof optionsRedis.client !== "object")) {
-          throw new Error("storage.options.client is required")
-        }
-
-        if (optionsRedis.invalidation && typeof optionsRedis.invalidation === "object") {
-          if (typeof optionsRedis.invalidation.invalidate !== "boolean") {
-            throw new Error("storage.options.invalidation.invalidate must be a boolean")
-          }
-
-          if (typeof optionsRedis.invalidation.referencesTTL === "number" && optionsRedis.invalidation.referencesTTL < 0) {
-            throw new Error("storage.options.invalidation.referencesTTL must be greater than 0")
-          }
-        }
-      } else {
-        throw new Error("storage.options must be defined")
-      }
-    } else {
-      throw new Error("storage.type must be memory or redis")
+  for (const prop of functionProperties) {
+    if (options[prop] && typeof options[prop] !== "function") {
+      throw new Error(`${prop} must be a function`)
     }
   }
 
-  return validated as ValidatedCacheOptions
+  return options
+}
+
+function validateStorage(options: CreateCacheOptions): CreateCacheOptions {
+  if (!options.storage) {
+    return {...options, storage: {type: "memory", options: {size: 1000}}}
+  } else if (options.storage.type === "memory") {
+    return {
+      ...options,
+      storage: {
+        ...options.storage,
+        options: validateMemoryStorage(options.storage.options)
+      }
+    }
+  } else if (options.storage.type === "redis") {
+    return {
+      ...options,
+      storage: {
+        ...options.storage,
+        options: validateRedisStorage(options.storage.options)
+      }
+    }
+  } else {
+    throw new Error("storage.type must be memory or redis")
+  }
+}
+
+function validateMemoryStorage(optionsMemory?: OptionsMemory): OptionsMemory {
+  const defaultOptions: OptionsMemory = {size: 1000}
+  const resultOptions = optionsMemory ?? defaultOptions
+
+  if (!resultOptions.size || typeof resultOptions.size !== "number") {
+    throw new Error("storage.options.size must be a number")
+  } else if (resultOptions.size && resultOptions.size < 0) {
+    throw new Error("storage.options.size must be greater than 0")
+  }
+  return resultOptions
+}
+
+function validateRedisStorage(optionsRedis: OptionsRedis): OptionsRedis {
+  if (!optionsRedis) {
+    throw new Error("storage.options must be defined")
+  }
+
+  if (!optionsRedis.client || typeof optionsRedis.client !== "object") {
+    throw new Error("storage.options.client is required")
+  }
+  if (typeof optionsRedis.invalidation !== "boolean") {
+    if (optionsRedis.invalidation && typeof optionsRedis.invalidation.invalidate !== "boolean") {
+      throw new Error("storage.options.invalidation.invalidate must be a boolean")
+    }
+
+    ensureNumberAndPositive(optionsRedis.invalidation?.referencesTTL, "storage.options.invalidation.referencesTTL", 60)
+  }
+  return optionsRedis
+}
+
+function ensureNumberAndPositive(value: unknown, propertyName: string, defaultValue?: number): number {
+  let resultValue = value
+  if (resultValue === undefined && defaultValue !== undefined) {
+    resultValue = defaultValue
+  }
+
+  if (typeof resultValue !== "number") {
+    throw new Error(`${propertyName} must be a number`)
+  } else if (resultValue < 0) {
+    throw new Error(`${propertyName} must be greater than 0`)
+  }
+
+  return resultValue
 }
